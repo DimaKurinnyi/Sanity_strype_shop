69eae89dd918c44493fbdd3f8ac88adc3e614b6a
Dima <dimakurinnoy8@gmail.com>
2022-12-14 16:56:17 +0100

    finish progect

--- ecommerce/context/StateContext.js
+++ ecommerce/context/StateContext.js
@@ -6,9 +6,12 @@ const Context = createContext();
 export const StateContext = ({ children }) => {
   const [showCart, setShowCart] = useState(false);
   const [cartItems, setCartItems] = useState([]);
-  const [totalPrice, setTotalPrice] = useState();
+  const [totalPrice, setTotalPrice] = useState(0);
   const [totalQuantities, setTotalQuantities] = useState(0);
   const [qty, setQty] = useState(1);
+
+  let foundProduct;
+  let index;
 
   const onAdd = (product, quantity) => {
     const checkProductInCart = cartItems.find((item) => item._id === product._id);
@@ -41,9 +44,37 @@ export const StateContext = ({ children }) => {
       return prevQty - 1;
     });
   };
+
+  const onRemove = (product) => {
+    foundProduct = cartItems.find((item) => item._id === product._id)
+    const newCartItems = cartItems.filter((item) => item._id !== product._id)
+
+    setTotalPrice((prevTotalPrice)=> prevTotalPrice - foundProduct.price*foundProduct.quantity)
+    setTotalQuantities((prevTotalQuantities)=> prevTotalQuantities - foundProduct.quantity)
+    setCartItems(newCartItems)
+  }
+
+  const toggleCartItemQuanitity =(id,value) =>{
+    foundProduct = cartItems.find((item) => item._id === id)
+    index = cartItems.findIndex((product) => product._id === id)
+    const newCartItem = cartItems.filter((item) => item._id !== id)
+
+    if (value === 'inc'){
+      setCartItems([...newCartItem,{...foundProduct,quantity:foundProduct.quantity + 1}])
+      setTotalPrice((prevTotalPrice)=> prevTotalPrice + foundProduct.price)
+      setTotalQuantities((prevTotalQuantities)=> prevTotalQuantities +1)
+
+    }else if (value === 'dec'){
+      if (foundProduct.quantity>1){
+        setCartItems([...newCartItem,{...foundProduct,quantity:foundProduct.quantity - 1}])
+      setTotalPrice((prevTotalPrice)=> prevTotalPrice - foundProduct.price)
+      setTotalQuantities((prevTotalQuantities)=> prevTotalQuantities - 1)
+      }
+  }
+}
   return (
     <Context.Provider
-      value={{ showCart, cartItems, totalPrice, totalQuantities, qty, incQty, decQty,onAdd,setShowCart }}>
+      value={{ showCart, cartItems, totalPrice, totalQuantities, qty, incQty, decQty,onAdd,setShowCart,toggleCartItemQuanitity,onRemove,setCartItems,setTotalPrice,setTotalQuantities }}>
       {children}
     </Context.Provider>
   );
--- ecommerce/pages/product/[slug].js
+++ ecommerce/pages/product/[slug].js
@@ -8,7 +8,15 @@ import {useStateContext} from '../../context/StateContext'
 function ProductDetails({ product, products }) {
   const { image, name, deteils, price } = product;
   const [index, setIndex] = useState(0);
-  const {decQty,incQty,qty,onAdd} = useStateContext()
+  const {decQty,incQty,qty,onAdd,setShowCart} = useStateContext()
+
+  const handlBuyNow=()=>{
+    onAdd(product,qty);
+
+    setShowCart(true)
+
+
+  }
   return (
     <div>
       <div className="product-detail-container">
@@ -18,7 +26,7 @@ function ProductDetails({ product, products }) {
           </div>
           <div className="small-images-container">
             {image?.map((item, i) => (
-              <img
+              <img key={i}
                 src={urlFor(item)}
                 className={i === index ? 'small-image selected-image' : 'small-image'}
                 onMouseEnter={() => setIndex(i)}
@@ -59,7 +67,7 @@ function ProductDetails({ product, products }) {
             <button type="button" className="add-to-cart" onClick={()=> onAdd(product,qty)}>
               Add to Cart
             </button>
-            <button type="button" className="buy-now" onClick="">
+            <button type="button" className="buy-now" onClick={handlBuyNow}>
               Buy Now
             </button>
           </div>
